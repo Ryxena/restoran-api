087d087bf8bfbe43eae9ed4bd0d7a95136d59e05
Ryxena <rubbenme@proton.me>
2024-03-11 21:07:11 +0700

    1.0.0

---
+++ database/migrations/2024_03_03_105710_create_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("description");
+            $table->bigInteger("price");
+            $table->unsignedBigInteger("category_id");
+            $table->foreign("category_id")->references("id")->on('category')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
